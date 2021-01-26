db0eff883543e5063adc233dad805f10e225cfe3
TBXark <tbxark@outlook.com>
2021-01-26 11:44:24 +0800

    file format

--- Example/main.c
+++ Example/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	char buf[1024];
-	FILE *file;
-	size_t nread;	
-	file = fopen("sub/name.text", "r");
-	if (file) {
-		while ((nread = fread(buf, 1, sizeof buf, file)) > 0);
-		fclose(file);
-	}
-	printf("Hello %s", buf);
+    char buf[1024];
+    FILE *file;
+    size_t nread;    
+    file = fopen("sub/name.text", "r");
+    if (file) {
+        while ((nread = fread(buf, 1, sizeof buf, file)) > 0);
+        fclose(file);
+    }
+    printf("Hello %s", buf);
 }
